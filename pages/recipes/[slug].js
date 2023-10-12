be764cb436f0c3878875ac91082b9e949121ffd2
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-10-12 14:13:18 +0200

    added routing

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,43 +1,56 @@
 // pages/[slug].js
-import { recipes } from '../../data';
-import React, {useState} from 'react';
+// import { recipes } from '../../data';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { getRecipeById } from '@/components/recipes';
 import Link from 'next/link';
 
-const Recipe = ({ recipe }) => {
-  const [showIngredients, setShowIngredients] = useState(false);
+const Recipe = () => {
+ const recipeId = useRouter().query.slug;
+ const recipe = getRecipeById(recipeId);
+  // const [showIngredients, setShowIngredients] = useState(false);
 
-  const toggleIngredients = () => {
-    setShowIngredients(!showIngredients);
-  };
+
+  // const toggleIngredients = () => {
+  //   setShowIngredients(!showIngredients);
+  // };
+
+console.log(recipe)
+  // const ingredientsArray = Object.values(recipe.ingredients);
 
   return (
     <div>
-    <h1>{recipe.title}</h1>
-    <p>{recipe.description}</p>
-    <button onClick={toggleIngredients}>
-      {showIngredients ? 'Hide Ingredients' : 'View Ingredients'}
-    </button>
-    {showIngredients && (
-      <div>
-        <h2>Ingredients for {recipe.title}</h2>
-        <ul>
-          {recipe.ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))}
-        </ul>
-      </div>
-    )}
-  </div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+
+      {showIngredients && (
+        <div>
+          <h2>Ingredients for {recipe.title}</h2>
+          <ul>
+            {ingredientsArray.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <button onClick={toggleIngredients}>
+        {showIngredients ? 'Hide Ingredients' : 'View Ingredients'}
+      </button>
+    </div>
   );
 };
 
 export default Recipe;
 
-export async function getServerSideProps(context) {
-  const { slug } = context.query;
-  const recipe = recipes.find((r) => r._id.toString() === slug);
+// export async function getServerSideProps(context) {
+//   const { slug } = context.query;
+//   // const recipe = recipes.find((r) => r._id.toString() === slug);
 
-  return {
-    props: { recipe },
-  };
-}
+//   if(slug){
+//   return {
+//     props: { recipe },
+//   };
+// }
+// }
+
