ab35c45bb1aa0b9b7a301ef666444e9f97568070
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-10-13 10:25:54 +0200

    Added image and instuctions

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
 import { run } from '@/fetching-data/data'
-import RecipesInstructions from '@/components/recipes-instructions';
 
 
 const Recipe = (props) => {
-
-  useEffect(() => {
-    console.log('props.recipes')
-  })
+const recipes = props.recipes
+console.log(recipes)
 
   return (
-    <RecipesInstructions recipes={props.recipes} />
+      <>
+     <h1>{recipes.title}</h1>
+     <img src={recipes.images[0]} alt={recipes._id} width={200} height={200}/>
+     <ul>
+      {recipes.instructions.map((res,index) => <li key={index}>{res}</li>)}
+     </ul>
+      </>
   );
 
 };
@@ -20,7 +23,7 @@ export default Recipe;
 export async function getStaticProps(context) {
   const recipeId = context.params.slug;
   const docs = await run();
-  const recipes1 = docs[0].find((recipe) => recipe._id === recipeId)
+  const recipes1 = docs.find((recipe) => recipe._id === recipeId)
 
   return {
     props: {
@@ -33,7 +36,7 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
   
   const docs = await run();
-  const paths =  docs[0].map((recipe) => ({params: {slug : recipe._id}}));
+  const paths =  docs.map((recipe) => ({params: {slug : recipe._id}}));
 
   return {
       paths: paths,
