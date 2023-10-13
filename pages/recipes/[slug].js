6171443e149a2157dfc49e1262e71e344ca72690
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-10-13 14:35:14 +0200

    added allergens elements

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react';
-import { run } from '@/fetching-data/data'
-
+import React from 'react';
+import { run } from '@/fetching-data/data';
 
 const Recipe = (props) => {
-const recipes = props.recipes
-console.log(recipes)
+  const { recipes } = props;
+
+  // Convert the ingredients object into an array of strings
+  const ingredientsArray = Object.entries(recipes.ingredients).map(([ingredient, amount]) => `${ingredient}: ${amount}`);
 
   return (
-      <>
-     <h1>{recipes.title}</h1>
-     <img src={recipes.images[0]} alt={recipes._id} width={200} height={200}/>
-     <ul>
-      {recipes.instructions.map((res,index) => <li key={index}>{res}</li>)}
-     </ul>
-      </>
+    <>
+      <h1>{recipes.title}</h1>
+      <img src={recipes.images[0]} alt={recipes._id} width={200} height={200} />
+      <p>{recipes.description}</p>
+      {recipes.allergens && recipes.allergens.length > 0 && (
+        <div>
+          <h2>Allergens</h2>
+          <ul>
+            {recipes.allergens.map((allergen, index) => (
+              <li key={index}>{allergen}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <h2>Ingredients</h2>
+      <ul>
+        {ingredientsArray.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h2>Instructions</h2>
+      <ol>
+        {recipes.instructions.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+    </>
   );
-
 };
 
 export default Recipe;
@@ -23,35 +43,24 @@ export default Recipe;
 export async function getStaticProps(context) {
   const recipeId = context.params.slug;
   const docs = await run();
-  const recipes1 = docs.find((recipe) => recipe._id === recipeId)
+  const recipes1 = docs.find((recipe) => recipe._id === recipeId);
 
   return {
     props: {
       recipes: recipes1,
-
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  
   const docs = await run();
-  const paths =  docs.map((recipe) => ({params: {slug : recipe._id}}));
+  const paths = docs.map((recipe) => ({ params: { slug: recipe._id } }));
 
   return {
-      paths: paths,
-      fallback: false
-      }
+    paths: paths,
+    fallback: false,
+  };
 }
 
-// export async function getServerSideProps(context) {
-//   const { slug } = context.query;
-//   // const recipe = recipes.find((r) => r._id.toString() === slug);
 
-//   if(slug){
-//   return {
-//     props: { recipe },
-//   };
-// }
-// }
 
