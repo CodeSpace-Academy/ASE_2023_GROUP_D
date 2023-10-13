8b6132a925a00b975ba029afc576410f11882378
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-10-13 14:26:34 +0200

    wrapped preview item with Link element

--- components/recipes/recipes-items.js
+++ components/recipes/recipes-items.js
@@ -1,33 +1,23 @@
 import styles from './recipes-items.module.css';
+import Link from 'next/link';
 
 function RecipesItems(props) {
-    const { id, title, description, prep, cook, category, servings, published, images } = props
-
-    //const recipeLink = `/recipes/${id}`;
+    const { id, title, description, prep, cook, category, servings, published, image } = props
 
     return (
-        <div>
-            <li className={styles.item}>
-            {/* <img src={'/' + image} alt={title} /> */}
-            <div className={styles.content}>
-                <div className={styles.summary}>
-                    <h1>{title}</h1>
-                    <img src={images} />
-                    <div>{description}</div>
-                    <div className={styles.address}>Preparation: {prep}minutes</div>
-                    <div className={styles.address}>Cook: {cook}minutes</div>
-                    <div>{category}</div>
-                    <div>{servings}</div>
-                    <div className={styles.date}>{published}</div>
-                    
-                </div>
-                {/* <div className={styles.actions}>
-                    <Button link={recipeLink}>
-                        <span>Preview Recipe</span>
-                    </Button>
-                </div> */}
-            </div>
-        </li>
+        <div className={styles.link}>
+            <Link href={`/recipes/${id}`}>
+                <li className={styles.item}>
+                    <h1> {title} </h1>
+                    <img src={image} alt={id} width={300} height={300} />
+                    {/* <div> {description} </div> */}
+                    <div className={styles.address}>Preparation: {prep}minutes </div>
+                    <div className={styles.address}>Cook: {cook}minutes </div>
+                    <div> {category} </div>
+                    <div> {servings} </div>
+                    <div className={styles.date}>{published} </div>
+                </li>
+            </Link>
         </div>
     )
 }
