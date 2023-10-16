ca1343e02d11619683b0dceb64744a880ef92762
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-10-16 14:04:16 +0200

    accepted changes from recipe-list.js file

--- components/recipes/recipes-list.js
+++ components/recipes/recipes-list.js
@@ -5,34 +5,6 @@ import styles from './recipes-list.module.css';
 
 function RecipeList(props) {
   const { recipes } = props;
-<<<<<<< HEAD
-  return (
-<div className={styles.recipeContainer}>
-<h1>Make your Own Recipe</h1>
-    <ul className={styles.list}>
-      
-      {recipes.map((recipe) => (
-        <RecipesItems
-          key={recipe._id}
-          id={recipe._id}
-          title={recipe.title}
-          image={recipe.images[0]}
-          description={recipe.description}
-          prep={recipe.prep}
-          cook={recipe.cook}
-          category={recipe.category}
-          servings={recipe.servings}
-          published={recipe.published}
-          images={recipe.images}
-        />
-
-      ))}
-    </ul>
-    </div>
-
-  )
-
-=======
   const [visibleRecipes, setVisibleRecipes] = useState(10); // Show the first 10 recipes
   const [remainingRecipes, setRemainingRecipes] = useState(recipes.length - visibleRecipes);
   const [isLoading, setIsLoading] = useState(false);
@@ -76,7 +48,6 @@ function RecipeList(props) {
       {isLoading && <p>Loading...</p>}
     </div>
   );
->>>>>>>>> Temporary merge branch 2
 }
 
 export default RecipeList;
