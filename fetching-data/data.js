0113eede5b058857171c77b5ef337fbad3a254e7
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-10-18 19:17:45 +0200

    added a skip filter to jump 100 recipes

--- fetching-data/data.js
+++ fetching-data/data.js
@@ -12,7 +12,30 @@ export const client = new MongoClient(uri, {
 	}
 });
 
-export async function run() {
+export async function run(page) {
+	try {
+	// Connect the client to the server    (optional starting in v4.7)
+	await client.connect();
+	// Send a ping to confirm a successful connection
+    const db = client.db("devdb");
+	await client.db("devdb").command({ ping: 1 });
+    const collection = db.collection("recipes"); 
+
+	const skip = (page - 1) * 100
+    // Use the find() method to retrieve data
+    const data = await collection.find({}).skip(skip).limit(100).toArray();
+	return data;
+
+    } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+	} finally {
+	// Ensures that the client will close when you finish/error
+	await client.close();
+	}
+}
+
+
+export async function runDetails(id) {
 	try {
 	// Connect the client to the server    (optional starting in v4.7)
 	await client.connect();
@@ -22,7 +45,7 @@ export async function run() {
     const collection = db.collection("recipes"); 
 
     // Use the find() method to retrieve data
-    const data = await collection.find({}).limit(100).toArray();
+    const data = await collection.find({_id: id}).toArray();
 	return data;
 
     } catch (error) {
