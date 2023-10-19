02c1babe6f96628bc82afb3a4bea23d891f7d54a
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-10-19 12:44:05 +0200

    destructured props

--- fetching-data/data.js
+++ fetching-data/data.js
@@ -1,99 +1,98 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = 
-`mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.mzswurt.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`
-;
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
 	serverApi: {
-	version: ServerApiVersion.v1,
-	strict: true,
-	deprecationErrors: true,
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
 	}
 });
 
-export async function run(page) {
+export async function run(page, limit) {
 	try {
-	// Connect the client to the server    (optional starting in v4.7)
-	await client.connect();
-	// Send a ping to confirm a successful connection
-    const db = client.db("devdb");
-	await client.db("devdb").command({ ping: 1 });
-    const collection = db.collection("recipes"); 
+		// Connect the client to the server    (optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		const db = client.db("devdb");
+		await client.db("devdb").command({ ping: 1 });
+		const collection = db.collection("recipes");
 
-	const skip = (page - 1) * 100
-    // Use the find() method to retrieve data
-    const data = await collection.find({}).skip(skip).limit(100).toArray();
-	return data;
+		const skip = (page - 1) * 100
+		// Use the find() method to retrieve data
+		const data = await collection.find({}).skip(skip).limit(100).toArray();
+		// return data.slice(0, limit);
+		return data
 
-    } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+	} catch (error) {
+		console.error("Failed to connect to MongoDB:", error);
 	} finally {
-	// Ensures that the client will close when you finish/error
-	await client.close();
+		// Ensures that the client will close when you finish/error
+		await client.close();
 	}
 }
 
 
 export async function runDetails(id) {
 	try {
-	// Connect the client to the server    (optional starting in v4.7)
-	await client.connect();
-	// Send a ping to confirm a successful connection
-    const db = client.db("devdb");
-	await client.db("devdb").command({ ping: 1 });
-    const collection = db.collection("recipes"); 
+		// Connect the client to the server    (optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		const db = client.db("devdb");
+		await client.db("devdb").command({ ping: 1 });
+		const collection = db.collection("recipes");
 
-    // Use the find() method to retrieve data
-    const data = await collection.find({_id: id}).toArray();
-	return data;
+		// Use the find() method to retrieve data
+		const data = await collection.find({ _id: id }).toArray();
+		return data;
 
-    } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+	} catch (error) {
+		console.error("Failed to connect to MongoDB:", error);
 	} finally {
-	// Ensures that the client will close when you finish/error
-	await client.close();
+		// Ensures that the client will close when you finish/error
+		await client.close();
 	}
 }
 
 export async function run1() {
 	try {
-	// Connect the client to the server    (optional starting in v4.7)
-	await client.connect();
-	// Send a ping to confirm a successful connection
-    const db = client.db("devdb");
-	await client.db("devdb").command({ ping: 1 });
-    const collection = db.collection("categories"); 
-    const data = await collection.find({}).toArray();
-    const dataArray = data.map(document => document.categories);
+		// Connect the client to the server    (optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		const db = client.db("devdb");
+		await client.db("devdb").command({ ping: 1 });
+		const collection = db.collection("categories");
+		const data = await collection.find({}).toArray();
+		const dataArray = data.map(document => document.categories);
 
-	return dataArray;
+		return dataArray;
 
-    } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+	} catch (error) {
+		console.error("Failed to connect to MongoDB:", error);
 	} finally {
-	// Ensures that the client will close when you finish/error
-	await client.close();
+		// Ensures that the client will close when you finish/error
+		await client.close();
 	}
 }
 
 export async function run2() {
 	try {
-	// Connect the client to the server    (optional starting in v4.7)
-	await client.connect();
-	// Send a ping to confirm a successful connection
-    const db = client.db("devdb");
-	await client.db("devdb").command({ ping: 1 });
-    const collection = db.collection("allergens"); 
-    const data = await collection.find({}).toArray();
-    const dataArray = data.map(document => document.allergens);
+		// Connect the client to the server    (optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		const db = client.db("devdb");
+		await client.db("devdb").command({ ping: 1 });
+		const collection = db.collection("allergens");
+		const data = await collection.find({}).toArray();
+		const dataArray = data.map(document => document.allergens);
 
-	return dataArray;
+		return dataArray;
 
-    } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+	} catch (error) {
+		console.error("Failed to connect to MongoDB:", error);
 	} finally {
-	// Ensures that the client will close when you finish/error
-	await client.close();
+		// Ensures that the client will close when you finish/error
+		await client.close();
 	}
 }
