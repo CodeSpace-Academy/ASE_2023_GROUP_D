a70398ce7552cd9e31e7eb3a44a958a09f79d4b4
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-10-27 14:01:16 +0200

    Delete filter.js
    deleted filter.js and created a new component filterSteps.js

---
+++ components/Navbar/Filter/FilterSteps.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+function FilterBySteps({ onFilter }) {
+  const [showInput, setShowInput] = useState(false);
+  const [filterValue, setFilterValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setFilterValue(e.target.value);
+    const numSteps = parseInt(e.target.value, 10);
+    if (!isNaN(numSteps) && numSteps > 0) {
+      onFilter(numSteps);
+    }
+    if (e.target.value.trim() === '') {
+      setShowInput(false);
+      onFilter(null);
+    }
+  };
+
+  return (
+    <div className="button-container">
+      {!showInput && <button onClick={() => setShowInput(true)}>Filter by Steps</button>}
+      {showInput && (
+        <div>
+          <div className="input-container"
+            type="number"
+            value={filterValue}
+            onChange={handleInputChange}
+            placeholder="Filter by Number of Steps"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FilterBySteps;
+
+
+
+
+
+
+
+;
+
+
+
+
+
+
+
