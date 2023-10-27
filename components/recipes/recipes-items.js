aab150067785c3c341cfd3b0b1c0b4a10998990e
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-10-27 14:06:06 +0200

    Updating recipe-list.js
    Updated the recipe-list.js by importing all the filters and created functions to handle the filters

--- components/recipes/recipes-items.js
+++ components/recipes/recipes-items.js
@@ -1,50 +1,51 @@
 import styles from './recipes-items.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../ui/button/button';
 
+
 function RecipesItems(props) {
-    const { id, title, prep, cook, category, servings, published, image, patcheNo } = props
+    const { id, title, prep, cook, category, servings, published, image, patcheNo, instructions } = props;
 
     const publishedDate = new Date(published);
     const formattedPublishedDate = publishedDate.toISOString().split('T')[0];
 
-    const viewRecipeLink = `/recipes/${patcheNo}/${id}`
+    const viewRecipeLink = `/recipes/${patcheNo}/${id}`;
+
+
 
     return (
         <div className={styles.link}>
+        <li className={styles.item}>
+            <img src={image} alt={id} width={400} height={200} className={styles.imageContainer} />
+            <div className={styles.title1}><h2> {title} </h2></div>
 
-            <li className={styles.item}>
-                <img src={image} alt={id} width={400} height={200} className={styles.imageContainer} />
-                <div className={styles.title1}><h2> {title} </h2></div>
-
-
-                {/* <div className={styles.address}>Preparation: {prep} minutes </div>
-                <div className={styles.address}>Cook: {cook} minutes </div> */}
-                <div className={styles.cookingContainer}>
-                    <div >
-                        <div className={styles.cookingTime}>
-                            <div className={styles.label}>Preparation:</div>
-                            <div className={styles.label}>Cooking time:</div>
-                        </div>
-                        <div className={styles.cookingTime}>
-                            <div className={styles.value}>{prep} mins</div>
-                            <div className={styles.value}>{cook} mins</div>
-
-                        </div>
+            <div className={styles.cookingContainer}>
+                <div>
+                    <div className={styles.cookingTime}>
+                        <div className={styles.label}>Preparation:</div>
+                        <div className={styles.label}>Cooking time:</div>
+                    </div>
+                    <div className={styles.cookingTime}>
+                        <div className={styles.value}>{prep} mins</div>
+                        <div className={styles.value}>{cook} mins</div>
                     </div>
                 </div>
+            </div>
 
-                <div> {category} </div>
-                <div> {servings} </div>
-                <div className={styles.date}>{formattedPublishedDate} </div>
-                <div className={styles.actions}>
-                    <Button link={viewRecipeLink} className={styles.viewRecipeButton}>
-                        <span className={styles.viewRecipeButtonText}>View Recipe</span>
-                    </Button>
-                </div>
-            </li>
-        </div>
+            <div> {category} </div>
+            <div> {servings} </div>
+            <div className={styles.date}>{formattedPublishedDate} </div>
+            <div className={styles.actions}>
+                <Button link={viewRecipeLink} className={styles.viewRecipeButton}>
+                    <span className={styles.viewRecipeButtonText}>View Recipe</span>
+                </Button>
+            </div>
+        </li>
+    </div>
     )
 }
 
 export default RecipesItems;
+
+
+
--- components/recipes/recipes-list.js
+++ components/recipes/recipes-list.js
@@ -1,39 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RecipesItems from "./recipes-items";
-//import LoadMoreButton from "../ui/button/button"; // Import the LoadMoreButton component
-import styles from './recipes-list.module.css'
+import FilterBySteps from "../Navbar/Filter/FilterSteps";
+import FilterByIngredients from '../Navbar/Filter/filterIngredients';
+import styles from './recipes-list.module.css';
 
 function RecipeList({ recipes, patcheNo }) {
+  const [filteredRecipes, setFilteredRecipes] = useState(null);
+  const [noRecipesMessage, setNoRecipesMessage] = useState(null);
+  const [ingredients, setIngredients] = useState([]); // Add state for ingredients
+
+  // Populate ingredients array when recipes change
+  useEffect(() => {
+    // Use runSearch function to fetch recipes from the server
+    const fetchData = async () => {
+      try {
+        const filter = {}; // Add your specific filter criteria here
+        const data = await runSearch(1, filter); // Assuming you're on page 1
+        const ingredientsArray = data.map(recipe => (
+          ...Object.entries(recipe.ingredients).map(([ingredient, amount]) => ({
+            name: ingredient,
+            amount: amount
+          }))
+        ));
+        setIngredients(ingredientsArray);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+
+  const handleFilterByIngredients = (ingredients) => {
+    if (ingredients) {
+      const ingredientList = ingredients.split(',').map(ingredient => ingredient.trim().toLowerCase());
+      const filtered = recipes.filter(recipe =>
+        ingredientList.every(filterIngredient =>
+          Object.entries(recipe.ingredients).some(([name, amount]) =>
+            `${name}: ${amount}`.toLowerCase().includes(filterIngredient)
+          )
+        )
+      );
+  
+      if (filtered.length > 0) {
+        setFilteredRecipes(filtered);
+        setNoRecipesMessage(null);
+      } else {
+        setFilteredRecipes(null);
+        setNoRecipesMessage(`No recipes with ingredients matching "${ingredients}" found.`);
+      }
+    } else {
+      setFilteredRecipes(null);
+      setNoRecipesMessage(null);
+    }
+  };
+  
+  
+
+  const handleFilterBySteps = (numSteps) => {
+    const filtered = recipes.filter(recipe => recipe.instructions.length === numSteps);
+    if (filtered.length > 0) {
+      setFilteredRecipes(filtered);
+      setNoRecipesMessage(null);
+    } else {
+      setFilteredRecipes(null);
+      setNoRecipesMessage(`No recipes with ${numSteps} steps found.`);
+    }
+  };
 
   return (
     <div className={styles.container}>
+      <FilterBySteps onFilter={handleFilterBySteps} />
+      <FilterByIngredients onFilter={handleFilterByIngredients} />
+      {noRecipesMessage && <p>{noRecipesMessage}</p>}
       <ul className={styles.list}>
-        {recipes.map((recipe) => (
-          <RecipesItems
-            key={recipe._id}
-            id={recipe._id}
-            patcheNo={patcheNo}
-            title={recipe.title}
-            image={recipe.images[0]}
-            description={recipe.description}
-            prep={recipe.prep}
-            cook={recipe.cook}
-            category={recipe.category}
-            servings={recipe.servings}
-            published={recipe.published}
-          />
-        ))} 
+        {filteredRecipes ? (
+          filteredRecipes.map((recipe) => (
+            <RecipesItems
+              key={recipe._id}
+              id={recipe._id}
+              patcheNo={patcheNo}
+              title={recipe.title}
+              image={recipe.images[0]}
+              description={recipe.description}
+              prep={recipe.prep}
+              cook={recipe.cook}
+              category={recipe.category}
+              servings={recipe.servings}
+              published={recipe.published}
+              instructions={recipe.instructions}
+            />
+          ))
+        ) : (
+          recipes.map((recipe) => (
+            <RecipesItems
+              key={recipe._id}
+              id={recipe._id}
+              patcheNo={patcheNo}
+              title={recipe.title}
+              image={recipe.images[0]}
+              description={recipe.description}
+              prep={recipe.prep}
+              cook={recipe.cook}
+              category={recipe.category}
+              servings={recipe.servings}
+              published={recipe.published}
+              instructions={recipe.instructions}
+            />
+          ))
+        )}
       </ul>
-      {/* {visibleRecipes < recipes.length && (
-        <LoadMoreButton onClick={loadMore} remaining={remainingRecipes} />
-      )}
-      {isLoading && <p>Loading...</p>} */}
     </div>
   );
 }
 
 export default RecipeList;
-
-
-
 
