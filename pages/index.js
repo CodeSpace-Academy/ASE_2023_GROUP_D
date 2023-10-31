19fc371a20eb31bbf73ade61d4feae85497247f4
Oteng29 <125869977+Oteng29@users.noreply.github.com>
2023-10-31 13:48:02 +0200

    changed the homepage layout
    - Added a login & sign up
    - Added a background image

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,70 @@
-import SearchBar from "@/components/text-search/auto-submission";
 import Link from "next/link";
 
 
 function Home(props) {
+  const buttonStyles = {
+    backgroundColor: '#ff0000',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    margin: '5px',
+  };
 
   return (
-    <>
-    <img src="images/BrandLogo.png" alt="logo" width={300} height={60} />
-
-      <Link href={`/recipes/1`}
+    <div
+      style={{
+        position: 'relative', // Container for relative positioning
+        height: '100vh',
+      }}
+    >
+      <div
         style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '50%', // Take up 50% of the width on the left
+          height: '100%',
+          background: 'rgba(0, 0, 0, 0.5)',
           display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          paddingTop: '30px'
-        }}>
-        <button className="theme-button">All Recipe</button>
-      </Link>
-
-      <SearchBar />
-    </>
-  )
+          zIndex: 2, // Ensure buttons are on top of the background
+        }}
+      >
+        <img src="/images/WhiteLogo.png" alt="logo" width={300} height={80} />
+        <Link href={`/recipes/1`} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '30px' }}>
+          <button style={buttonStyles}>All Recipe</button>
+        </Link>
+        {/* Login and Sign Up buttons with links */}
+        <Link href="/login" style={{ textDecoration: 'none' }}>
+          <button style={buttonStyles}>Login</button>
+        </Link>
+        <Link href="/signup" style={{ textDecoration: 'none' }}>
+          <button style={buttonStyles}>Sign Up</button>
+        </Link>
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: '0%', // Take up the right 50% of the width
+          width: '100%', // Take up 50% of the width on the right
+          height: '100%',
+          zIndex: 1,
+          backgroundImage: 'url("/images/homepage-pic.jpg")',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+          backgroundColor: 'black',
+        }}
+      >
+        
+      </div>
+    </div>
+  );
 }
 
-
-export default Home
-
+export default Home;
