1c4aee8a457a43d2831c445e336c474a9f212fe4
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-01 09:55:30 +0200

    added a favourites section

--- components/recipes/recipes-items.js
+++ components/recipes/recipes-items.js
@@ -1,10 +1,12 @@
 import styles from './recipes-items.module.css';
 import React from 'react';
 import Button from '../ui/button/button';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function RecipesItems(props) {
     const { id, title, prep, cook, category, servings, published, image, patcheNo, description, favRecipes } = props
+    const [favToggle, setFavToggle] = useState(false)
+    const [removeFromFav, setRemoveFromFav] = useState(true)
 
     const favRecipeIds = favRecipes.map((recipe) => recipe._id)
 
@@ -24,8 +26,8 @@ function RecipesItems(props) {
         published: published
     }
 
-    function addCommentHandler(recipeData) {
-        fetch('/api/favourites', {
+    async function addToFavourite(recipeData) {
+        await fetch('/api/favourites', {
             method: 'POST',
             body: JSON.stringify(recipeData),
             headers: {
@@ -33,44 +35,59 @@ function RecipesItems(props) {
             },
         })
             .then((response) => response.json())
-            .then((data) => console.log(data.recipes._id));
+            .then((data) => {
+                // console.log(data)
+                setFavToggle(!favToggle)
+            });
     }
 
-//   console.log(favRecipes)
+    async function removeFromFavourite(recipeData) {
+        await fetch(`/api/favourites?id=${id}`, {
+            method: 'DELETE',
+            body: JSON.stringify(recipeData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        setFavToggle(!favToggle)
+        setRemoveFromFav(false)
+    } 
+
+    //   console.log(favRecipes)
     return (
-        <div className={styles.link}>
+        <>{removeFromFav &&
+            <div className={styles.link}>
 
-            <li className={styles.item}>
-                <img src={image} alt={id} width={400} height={200} className={styles.imageContainer} />
-                <div className={styles.title1}><h2> {title} </h2></div>
+                <li className={styles.item}>
+                    <img src={image} alt={id} width={400} height={200} className={styles.imageContainer} />
+                    <div className={styles.title1}><h2> {title} </h2></div>
 
+                    <div className={styles.cookingContainer}>
+                        <div >
+                            <div className={styles.cookingTime}>
+                                <div className={styles.label}>Preparation:</div>
+                                <div className={styles.label}>Cooking time:</div>
+                            </div>
+                            <div className={styles.cookingTime}>
+                                <div className={styles.value}>{prep} mins</div>
+                                <div className={styles.value}>{cook} mins</div>
 
-                {/* <div className={styles.address}>Preparation: {prep} minutes </div>
-                <div className={styles.address}>Cook: {cook} minutes </div> */}
-                <div className={styles.cookingContainer}>
-                    <div >
-                        <div className={styles.cookingTime}>
-                            <div className={styles.label}>Preparation:</div>
-                            <div className={styles.label}>Cooking time:</div>
-                        </div>
-                        <div className={styles.cookingTime}>
-                            <div className={styles.value}>{prep} mins</div>
-                            <div className={styles.value}>{cook} mins</div>
-
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div> {category} </div>
-                <div> {servings} </div>
-                <div className={styles.date}>{formattedPublishedDate} </div>
-                <div className={styles.actions}>
-                    <Button link={`/recipes/${patcheNo}/${id}`} className={styles.viewRecipeButton}>
-                        <span className={styles.viewRecipeButtonText}>View Recipe</span>
-                    </Button>
-                    <button onClick={() => addCommentHandler(recipeToBeInsertedToFav)}>{favRecipeIds.includes(id) ?'Rev From Fav' :'Add To Fav'}</button>
-                </div>
-            </li>
-        </div>
+                    <div> {category} </div>
+                    <div> {servings} </div>
+                    <div className={styles.date}>{formattedPublishedDate} </div>
+                    <div className={styles.actions}>
+                        <Button link={`/recipes/${patcheNo}/${id}`} className={styles.viewRecipeButton}>
+                            <span className={styles.viewRecipeButtonText}>View Recipe</span>
+                        </Button>
+                        {favRecipeIds.includes(id) ? <button onClick={() => removeFromFavourite({ _id: id })}>Rev From Fav</button>
+                            : <button onClick={() => addToFavourite(recipeToBeInsertedToFav)}>{favToggle ? 'Rev From Fav' : 'Add To Fav'}</button>}
+                    </div>
+                </li>
+            </div>
+        }</>
     )
 }
 
