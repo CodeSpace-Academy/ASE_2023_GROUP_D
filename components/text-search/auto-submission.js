79001642347d69e7084b83a22d246837b105ba5b
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-04 21:24:17 +0200

    adding a timer for auto search

--- components/text-search/auto-submission.js
+++ components/text-search/auto-submission.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import styles from "@/components/text-search/searchBar.module.css"
 import Link from "next/link";
 
-function SearchBar({search}) {
+function SearchBar({ search }) {
   const [query, setQuery] = useState("");
   const [handel, setHandelSS] = useState('')
   const delay = 5000;
@@ -19,48 +19,57 @@ function SearchBar({search}) {
 
   //   // Create a new object by spreading the existing query parameters
   //   const newQuery = { ...query };
-  
+
   //   // Add or update a new query parameter
   //   newQuery.Prep = handel;
-  
+
   //   // Use router.push to navigate to the updated URL with the new query parameter
   //   router.push({
   //     pathname,
   //     query: newQuery,
   //   });
   // }
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   const navigateToNewPage = () => {
-  //     router.push(`/Search/${query}`); // Replace '/new-page' with the URL of the new page
-  //   };
+    if (query) {
+      const navigateToNewPage = () => {
+        router.push(`/Search/${query}`); // Replace '/new-page' with the URL of the new page
+      };
 
-  //   const timeoutId = setTimeout(navigateToNewPage, delay);
+      const timeoutId = setTimeout(navigateToNewPage, delay);
 
-  //   return () => {
-  //     clearTimeout(timeoutId); // Clear the timeout if the component unmounts before the delay is reached
-  //   };
-  // }, [router, query, delay]);
+      return () => {
+        clearTimeout(timeoutId); // Clear the timeout if the component unmounts before the delay is reached
+        // timeoutId === 0 && setQuery('')
+      };
+    }
+  }, [router, query, delay]);
 
-  function handelSS (event){
+  useEffect(() => {
+    return () => {
+      setQuery('');
+    };
+  }, [router]);
+
+  function handelSS(event) {
     setHandelSS(event.target.value)
   }
 
   return (
     <section>
       <div className={styles.container}>
-      <div className={styles.searchBar}>
-        <input className={styles.input} type="text" placeholder="Enter text ..." value={query} onChange={handleInputChange}/>
-        <button className={styles.button}>Search</button>
+        <div className={styles.searchBar}>
+          <input className={styles.input} type="text" placeholder="Enter text ..." value={query} onChange={handleInputChange} />
+          <button className={styles.button}>Search</button>
+        </div>
       </div>
-    </div>
-    <select value={handel} onChange={handelSS}>
-      <option value={1}>Ascending</option>
-      <option value={-1}>Descending</option>
-    </select>
-    <Link href={`/Search/${query}?Prep=${handel}`}> 
-    <button>submit</button>
-    </Link>
+      <select value={handel} onChange={handelSS}>
+        <option value={1}>Ascending</option>
+        <option value={-1}>Descending</option>
+      </select>
+      <Link href={`/Search/${query}?Prep=${handel}`}>
+        <button>submit</button>
+      </Link>
     </section>
   );
 }
