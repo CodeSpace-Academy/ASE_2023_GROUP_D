ae62bf290ad01753e3862e74fa90a09ae9ce623b
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-04 21:23:37 +0200

    show a massage if no recipes match search

--- pages/Search/[search].js
+++ pages/Search/[search].js
@@ -1,18 +1,21 @@
-import { runFilter2 } from "@/fetching-data/data";
+import { runFilter2, runFav } from "@/fetching-data/data";
 import RecipeList from "@/components/recipes/recipes-list";
 import Navbar from "@/components/header/navbar";
 import SearchBar from "@/components/text-search/auto-submission";
 import { useRouter } from "next/router";
 
-function Search({ filteredCharacters }) {
+function Search({ filteredCharacters, favRecipes}) {
     const router = useRouter();
     const {search} = router.query
+
+    console.log(search)
+    console.log(filteredCharacters)
     return (
         <>
             <Navbar />
             <SearchBar  search={search} />
             {/* Render the 'RecipeList' component, passing in the first 20 items of the 'filteredCharacters' array and 'patcheNo' as a prop. */}
-            <RecipeList recipes={filteredCharacters.slice(0, 20)} patcheNo={1} />
+            {filteredCharacters.length > 0 ? <RecipeList recipes={filteredCharacters.slice(0, 20)} patcheNo={1} favRecipes={favRecipes}/>: <h2>No Matching Recipes Based On Search</h2>}
         </>
     )
 }
@@ -21,19 +24,19 @@ export async function getServerSideProps(context) {
 
     const searchChar = context.params.search
     const Prep = parseInt(context.query.Prep)
-    console.log(Prep)
     {/* Display a list of recipes, showing the first 20 recipes from the search results. */ }
     const filterCharacter = { title: { $regex: searchChar, $options: 'i' }}
     const sortCharacter = Prep ? {prep: Prep } : {}
 
-    console.log(sortCharacter)
     // This is the list of recipes that were found based on the user's search.
     const filteredCharacters = await runFilter2(1, filterCharacter, sortCharacter)
+    const favRecipes = await runFav(1);
 
     // Send the list of recipes to be displayed on the webpage.
     return {
         props: {
             filteredCharacters,
+            favRecipes,
         },
     };
 }
