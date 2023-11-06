1ec35e786b1124309e229f2bcf414e05262593a0
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-06 06:05:00 +0200

    added queies for ingrediets and categories

--- pages/Search/[search].js
+++ pages/Search/[search].js
@@ -6,20 +6,21 @@ import styles from '@/components/header/summary.module.css'
 import { useState } from "react";
 import { useRouter } from "next/router";
 
+
 function Search({ filteredCharacters, favRecipes, categories }) {
     const router = useRouter();
     const { search } = router.query
     const [loadmore, setLoadMore] = useState(filteredCharacters.length)
     const [loadData, setLoadData] = useState(20)
 
-    // console.log(search)
+    console.log(search)
     // console.log(filteredCharacters)
     return (
         <>
             <Navbar />
             <SearchBar search={search} categories={categories} />
             {/* Render the 'RecipeList' component, passing in the first 20 items of the 'filteredCharacters' array and 'patcheNo' as a prop. */}
-            {filteredCharacters.length > 0 ? <RecipeList recipes={filteredCharacters.slice(0, loadData)} patcheNo={1} favRecipes={favRecipes} /> : <h2>No Matching Recipes Based On Search</h2>}
+            {filteredCharacters.length > 0 ? <RecipeList recipes={filteredCharacters.slice(0, loadData)} patcheNo={1} favRecipes={favRecipes} search={search} /> : <h2>No Matching Recipes Based On Search</h2>}
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <button onClick={() => {
@@ -44,19 +45,17 @@ export async function getServerSideProps(context) {
     const searchChar = context.params.search
     const Tags = context.query.Tags
     const Prep = parseInt(context.query.Prep)
+    const Categories = context.query.Categories
+    const Ingredients = context.query.Ingredients
     const tagsArray = Tags.split(',');
-    // console.log(tagsArray)
-    // console.log(Prep)
-    {/* Display a list of recipes, showing the first 20 recipes from the search results. */ }
-    const filterCharacter = { title: { $regex: searchChar, $options: 'i' }, tags: { $all: tagsArray } }
-
-   
-    // context.query.search && { ...finalSearchString, ...{ title: { $regex: searchChar, $options: 'i' } } }
-    // context.query.Tags && { ...finalSearchString, ...{ tags: { $all: tagsArray } } }
+    const ingredientsArray = Ingredients.split(',');
+  
     const sortCharacter = Prep ? { prep: Prep } : {}
     searchChar ? finalSearchString.title = { $regex: searchChar, $options: 'i' } : undefined
     Tags ? finalSearchString.tags = { $all: tagsArray } : undefined
-    
+    Categories ? finalSearchString.category = Categories : undefined
+    Ingredients ? ingredientsArray.map((ingredient)=> finalSearchString[`ingredients.${ingredient}`] = {$exists: true}) : undefined
+
     // This is the list of recipes that were found based on the user's search.
     const filteredCharacters = await runFilter2(1, finalSearchString, sortCharacter)
     const favRecipes = await runFav(1);
