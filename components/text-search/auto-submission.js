47e5a960cf217a92444074a3a1c0ae29351c13c8
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-06 06:10:08 +0200

    added select tags for ingredients, categories, tags, and order

--- components/text-search/auto-submission.js
+++ components/text-search/auto-submission.js
@@ -2,15 +2,18 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "@/components/text-search/searchBar.module.css"
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass as searchIcon } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar({ search, categories }) {
   const [query, setQuery] = useState("");
   const [backUpQuery, setBackUpQuery] = useState(search);
-  const [handel, setHandelSS] = useState('')
+  const [prep, setHandlePrep] = useState('')
   const [tags, setTags] = useState([])
-  const delay = 5000;
+  const [ingredients, setIngredients] = useState([])
+  const [category, setCategory] = useState('')
   const router = useRouter();
-  console.log(categories[0])
+  const delay = 5000;
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
@@ -20,7 +23,7 @@ function SearchBar({ search, categories }) {
 
     if (query) {
       const navigateToNewPage = () => {
-        router.push(`/Search/${query ? query : backUpQuery}?Prep=${handel}&Tags=${tags}`); // Replace '/new-page' with the URL of the new page
+        router.push(`/Search/${query ? query : backUpQuery}?Prep=${prep}&Tags=${tags}&Categories=${category}&Ingredients=${ingredients}`); // Replace '/new-page' with the URL of the new page
       };
 
       const timeoutId = setTimeout(navigateToNewPage, delay);
@@ -37,8 +40,8 @@ function SearchBar({ search, categories }) {
     };
   }, [router]);
 
-  function handelSS(event) {
-    setHandelSS(event.target.value)
+  function handelSortByPrep(event) {
+    setHandlePrep(event.target.value)
   }
 
   function handleTag(event) {
@@ -49,42 +52,96 @@ function SearchBar({ search, categories }) {
     setTags(tags.filter((tag) => tag !== event.target.value))
   }
 
+  function handleIngredients(event) {
+    setIngredients((prev) => [...prev, event.target.value])
+  }
+
+  function handleDeleteIngredients(event) {
+    setIngredients(ingredients.filter((ingredient) => ingredient != event.target.value))
+  }
+
+  function handleCategory(event) {
+    setCategory(event.target.value)
+  }
+
+  function handleDeleteAllFilters(){
+    setTags([])
+    setCategory('')
+    setIngredients([])
+    setHandlePrep('')
+  }
+
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column' }} className={styles.container}>
       <div className={styles.container}>
         <div className={styles.searchBar}>
+          <FontAwesomeIcon icon={searchIcon} size="lg" color="black" style={{ paddingRight: '10px' }} />
           <input className={styles.input} type="text" placeholder="Enter text ..." value={query} onChange={handleInputChange} />
-          <button className={styles.button}>Search</button>
         </div>
       </div>
-      <select value={tags[tags.length - 1]} onChange={handleTag}>
-        <option value={'Beans'}>Beans</option>
-        <option value={'Vegetable'}>Vegetable</option>
-        <option value={'Oven'}>Oven</option>
-        <option value={'Dessert'}>Dessert</option>
-        <option value={'Healthy'}>Healthy</option>
-        <option value={'Fruit'}>Fruit</option>
-      </select>
 
-      <select value={handel} onChange={handelSS}>
-        <option value={1}>Ascending</option>
-        <option value={-1}>Descending</option>
-      </select>
+      {router.pathname.includes('/Search/') &&
+        <>
+          <div style={{ display: 'flex' }}>
+            <label><h5>SortByTag : </h5></label>
+            <select value={tags[tags.length - 1]} onChange={handleTag}>
+              <option value={'Beans'}>Beans</option>
+              <option value={'Vegetable'}>Vegetable</option>
+              <option value={'Oven'}>Oven</option>
+              <option value={'Dessert'}>Dessert</option>
+              <option value={'Healthy'}>Healthy</option>
+              <option value={'Fruit'}>Fruit</option>
+            </select>
+            {tags.map((tag, index) => {
+              return (<button key={index} onClick={handleDeleteTag} value={tag}>{tag}</button>)
+            })}
+          </div>
 
-      <select value={handel} onChange={handelSS}>
-        {categories[0].map((category, index)=> <option key={index} value={category}>{category}</option> )}
-      </select>
-     
-      <Link href={`/Search/${query ? query : backUpQuery}?Prep=${handel}&Tags=${tags}`}>
-        <button>submit</button>
-      </Link>
+          <div style={{ display: 'flex' }}>
+            <label><h5>SortByOrd : </h5></label>
+            <select value={prep} onChange={handelSortByPrep}>
+              <option value={1}>Ascending</option>
+              <option value={-1}>Descending</option>
+            </select>
+          </div>
 
-      {tags.map((tag, index) => {
-        return (<button key={index} onClick={handleDeleteTag} value={tag}>{tag}</button>)
-      })}
+          <div style={{ display: 'flex' }}>
+            <label><h5>SortByCat : </h5></label>
+            <select value={category} onChange={handleCategory}>
+              {categories[0].map((category, index) => <option key={index} value={category}>{category}</option>)}
+            </select>
+          </div>
+
+          <div style={{ display: 'flex' }}>
+            <label><h5>SortByIng : </h5></label>
+            <select value={ingredients[ingredients.length - 1]} onChange={handleIngredients}>
+              <option value={'onion'}>onion</option>
+              <option value={'garlic'}>garlic</option>
+              <option value={'potatoes'}>potatoes</option>
+              <option value={'butter'}>butter</option>
+              <option value={'milk'}>milk</option>
+              <option value={'egg'}>egg</option>
+              <option value={'mushrooms'}>mushrooms</option>
+              <option value={'salt'}>salt</option>
+              <option value={'ricotta cheese'}>ricotta cheese</option>
+            </select>
+            {ingredients.map((ingredient, index) => {
+              return (<button key={index} onClick={handleDeleteIngredients} value={ingredient}>{ingredient}</button>)
+            })}
+          </div>
+
+          <Link href={`/Search/${search}?Prep=${prep}&Tags=${tags}&Categories=${category}&Ingredients=${ingredients}`}>
+            <button>submit</button>
+          </Link>
+          <Link href={`/Search/${search}?Prep=${prep}&Tags=${tags}&Categories=${category}&Ingredients=${ingredients}`}>
+            <button onClick={handleDeleteAllFilters}>Clear All Filters</button>
+          </Link>
+        </>
+      }
+
     </div>
 
-    
+
   );
 }
 
