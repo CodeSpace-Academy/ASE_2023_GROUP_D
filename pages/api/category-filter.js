9c8416c5558d7f9cc1306af5f2a9441eb606ace3
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-11-08 17:26:07 +0200

    commented out component

--- pages/api/category-filter.js
+++ pages/api/category-filter.js
@@ -1,18 +1,18 @@
 // pages/api/recipes.js
-import connectMongodb from '../../libs/mongodb'; // Your database connection code
+// import connectMongodb from '../../libs/mongodb'; // Your database connection code
 
-export default async (req, res) => {
-  const { category } = req.query;
+// export default async (req, res) => {
+//   const { category } = req.query;
 
-  try {
-    const db = connectMongodb();
-    const collection = db.collection('recipes');
+//   try {
+//     const db = connectMongodb();
+//     const collection = db.collection('recipes');
 
-    // Fetch recipes based on the category parameter
-    const recipes = await collection.find({ category }).toArray();
+//     // Fetch recipes based on the category parameter
+//     const recipes = await collection.find({ category }).toArray();
 
-    res.status(200).json(recipes);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching recipes' });
-  }
-};
+//     res.status(200).json(recipes);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Error fetching recipes' });
+//   }
+// };
