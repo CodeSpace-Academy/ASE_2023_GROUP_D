7f3762fd7c3f59145a00652e2d1cbef4b95b4527
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-14 09:16:47 +0200

    Added sort by ingredients component

---
+++ components/sorting/sortByIngredients.js
@@ -0,0 +1,33 @@
+function SortByIngrediets({ setIngredients, ingredients }) {
+
+    function handleIngredients(event) {
+        setIngredients((prev) => [...prev, event.target.value])
+    }
+
+    function handleDeleteIngredients(event) {
+        setIngredients(ingredients.filter((ingredient) => ingredient != event.target.value))
+    }
+
+    return (
+        <div style={{ display: 'flex' }}>
+            <label><h5 style={{color: 'white'}}>SortByIng : </h5></label>
+            <select value={ingredients[ingredients.length - 1]} onChange={handleIngredients}>
+                <option value={'onion'}>onion</option>
+                <option value={'garlic'}>garlic</option>
+                <option value={'potatoes'}>potatoes</option>
+                <option value={'butter'}>butter</option>
+                <option value={'milk'}>milk</option>
+                <option value={'egg'}>egg</option>
+                <option value={'mushrooms'}>mushrooms</option>
+                <option value={'salt'}>salt</option>
+                <option value={'ricotta cheese'}>ricotta cheese</option>
+            </select>
+
+            {ingredients.map((ingredient, index) => {
+                return (<button key={index} onClick={handleDeleteIngredients} value={ingredient}>{ingredient}</button>)
+            })}
+        </div>
+    )
+}
+
+export default SortByIngrediets;
