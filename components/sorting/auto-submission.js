38229b795973080ec39c34e8f183cb9860236d08
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-16 14:15:39 +0200

    added logic to which filter is mounted to search query and got the filter values after refresh from url

--- components/sorting/auto-submission.js
+++ components/sorting/auto-submission.js
@@ -7,15 +7,18 @@ import { faMagnifyingGlass as searchIcon } from "@fortawesome/free-solid-svg-ico
 import FilterByTag from "./filterByTag";
 import FilterByIngrediets from "./filterByIngredients";
 import FilterByCategory from "./filterByCategory";
+import FilterBySteps from "./filterBySteps";
 
-function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, history }) {
+function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, history, filterByTags, filterByIngredients, categoryfilter, filterBySteps }) {
   const [query, setQuery] = useState();
   const [backUpQuery, setBackUpQuery] = useState(searchChar)
-  const [tags, setTags] = useState([])
-  const [ingredients, setIngredients] = useState([])
-  const [category, setCategory] = useState('')
+  const [tags, setTags] = useState(filterByTags)
+  const [ingredients, setIngredients] = useState(filterByIngredients)
+  const [category, setCategory] = useState(categoryfilter)
   const [filterToggle, setFilterToggle] = useState(false)
+  const [numSteps, setNumSteps] = useState(filterBySteps)
   const router = useRouter();
+  const {asPath} = router
   const delay = 5000;
 
   const handleInputChange = (event) => {
@@ -26,7 +29,7 @@ function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, hi
 
     if (query) {
       const navigateToNewPage = () => {
-        router.push(`/recipes/${pageNo}/?search=${query ? query : backUpQuery}`); // Replace '/new-page' with the URL of the new page
+        router.push(`/recipes/1/?search=${query ? query : backUpQuery}`); // Replace '/new-page' with the URL of the new page
       };
 
       const timeoutId = setTimeout(navigateToNewPage, delay);
@@ -44,21 +47,15 @@ function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, hi
   }, [router]);
 
 
-  function handleDeleteAllFilters() {
-    setTags([])
-    setCategory('')
-    setIngredients([])
-  }
-
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <div className={styles.searchBar}>
         <FontAwesomeIcon icon={searchIcon} size="lg" color="black" style={{ paddingRight: '10px', paddingTop: '30px' }} />
         <input className={styles.input} onClick={() => setFilterToggle(!filterToggle)} type="text" placeholder="Enter text ..." value={query} onChange={handleInputChange} />
         <select>
-         {history.map((data, index)=> {
+          {history.map((data, index) => {
             return <option key={index} value={data}>{data}</option>
-         })}
+          })}
         </select>
       </div>
 
@@ -66,15 +63,18 @@ function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, hi
         <FilterByTag setTags={setTags} tags={tags} />
         <FilterByIngrediets setIngredients={setIngredients} ingredients={ingredients} />
         <FilterByCategory categories={categories} category={category} setCategory={setCategory} />
+        <FilterBySteps setNumSteps={setNumSteps} numSteps={numSteps} />
       </>
-      <button onClick={()=> setIsSorting(!isSorting)}>Close</button>
-               
-          <Link href={`/recipes/${pageNo}/?${backUpQuery && 'search='}${query ? query : backUpQuery}&tags=${tags}&categories=${category}&ingredients=${ingredients}`}>
-            <button>filter</button>
-          </Link>
-          {/* <Link href={`/recipes/${pageNo}/?Prep=${prep}&Tags=${tags}&Categories=${category}&Ingredients=${ingredients}`}>
-            <button onClick={handleDeleteAllFilters}>Clear All Filters</button>
-          </Link> */}
+      <div style={{display: 'flex', width: 'fit-content'}}>
+      <Link href={`/recipes/1/?${backUpQuery ? `search=${query ? query : backUpQuery}&`: ''}tags=${tags}&categories=${category}&ingredients=${ingredients}&steps=${numSteps}`}>
+        <button>filter</button>
+      </Link>
+      <Link href={`/recipes/1${asPath.includes('?search=')  ? `/?search=${backUpQuery}`: '' }`}>
+        <button >Clear All Filters</button>
+      </Link>
+      </div>
+      <button onClick={() => setIsSorting(!isSorting)}>Close</button>
+     
     </div>
 
 
