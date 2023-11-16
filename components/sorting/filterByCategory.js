952828c4310ef1c7aab8c3f452abff621e0f1f53
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-11-16 14:09:20 +0200

    Added a category filter component

--- components/sorting/filterByCategory.js
+++ components/sorting/filterByCategory.js
@@ -1,15 +1,22 @@
-function FilterByCategory({ categories, setCategory, category }) {
+import { useState } from "react";
 
+function FilterByCategory({ categories, setCategory, category }) {
+    const [chosenValue, setChosenValue] = useState(category)
 
     function handleCategory(event) {
         setCategory(event.target.value)
+        setChosenValue(event.target.value)
     }
 
     return (
         <div style={{ display: 'flex' }}>
-            <label><h5 style={{color: 'white'}}>FilterByCat : </h5></label>
-            <select value={category} onChange={handleCategory}>
-                {categories[0].map((category, index) => <option key={index} value={category}>{category}</option>)}
+            <label><h5 style={{ color: 'white' }}>FilterByCat : </h5></label>
+            <select value={chosenValue} onChange={handleCategory}>
+                <option value={undefined}>Choose...</option>
+                {categories[0].map((category, index) => {
+                    return <option key={index} value={category}>{category}</option>
+                })
+                }
             </select>
         </div>
     )
