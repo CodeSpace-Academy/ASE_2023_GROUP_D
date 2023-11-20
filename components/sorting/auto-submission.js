735793369de5c2f5e7578764c372c851c2c501a2
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-11-20 11:25:02 +0200

    Added long query button
    When characters are less than 10 the search should automatically auto submit otherwise when its more than 10 characters a search button should appear and the search should be submitted using the button.

--- components/sorting/auto-submission.js
+++ components/sorting/auto-submission.js
@@ -17,6 +17,8 @@ function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, hi
   const [category, setCategory] = useState(categoryfilter)
   const [filterToggle, setFilterToggle] = useState(false)
   const [numSteps, setNumSteps] = useState(filterBySteps)
+  const [showSubmitButton, setShowSubmitButton] = useState(false);
+
   const router = useRouter();
   const {asPath} = router
   const delay = 5000;
@@ -26,25 +28,39 @@ function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, hi
   };
 
   useEffect(() => {
+    if (query && query.length > 10) {
+      setShowSubmitButton(true);
 
-    if (query) {
       const navigateToNewPage = () => {
-        router.push(`/recipes/1/?search=${query ? query : backUpQuery}`); // Replace '/new-page' with the URL of the new page
+        router.push(`/recipes/1/?search=${query ? query : backUpQuery}`);
       };
 
       const timeoutId = setTimeout(navigateToNewPage, delay);
 
       return () => {
-        clearTimeout(timeoutId); // Clear the timeout if the component unmounts before the delay is reached
+        clearTimeout(timeoutId);
       };
+    } else if (query && query.length <= 10) {
+      setShowSubmitButton(false);
+
+      const navigateToNewPage = () => {
+        router.push(`/recipes/1/?search=${query ? query : backUpQuery}`);
+      };
+
+      navigateToNewPage(); // Auto submit for queries shorter than or equal to 10 characters
     }
   }, [router, query, delay]);
+
 
   useEffect(() => {
     return () => {
       setQuery('');
     };
   }, [router]);
+
+  const handleButtonClick = () => {
+    router.push(`/recipes/1/?search=${query ? query : backUpQuery}`);
+  };
 
 
   return (
@@ -57,6 +73,11 @@ function SearchBar({ categories, pageNo, searchChar, setIsSorting, isSorting, hi
             return <option key={index} value={data}>{data}</option>
           })}
         </select>
+        {showSubmitButton && (
+        <button onClick={handleButtonClick}>
+          Submit
+        </button>
+      )}
       </div>
 
       <>
