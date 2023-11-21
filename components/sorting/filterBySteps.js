4da858885f13b6cc99b7a49fa51cf5997132dffe
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-11-21 10:27:36 +0200

    styled the dropdown menu for tags

--- components/sorting/filterBySteps.js
+++ components/sorting/filterBySteps.js
@@ -1,3 +1,5 @@
+import styles from "./searchBar.module.css"
+
 function FilterBySteps({ setNumSteps, numSteps }) {
 
 
@@ -8,7 +10,9 @@ function FilterBySteps({ setNumSteps, numSteps }) {
     return (
         <div style={{ display: 'flex' }}>
             <label htmlFor="numSteps"><h5 style={{ color: 'white' }}>FilterByStep : </h5></label>
-            <input type="number" id="numSteps" value={numSteps} onChange={handleSteps}></input>
+            <div className={styles.containerdropdown}> 
+                <input type="number" id="numSteps" value={numSteps} onChange={handleSteps}></input>
+            </div>
         </div>
     )
 }
