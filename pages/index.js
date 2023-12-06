a473055d19ea6f8022d052986310aac856735b55
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-12-06 15:51:40 +0200

    Update index.js
    removed inline styling

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,6 @@ import LoadingState from "@/components/Loading/loading-state";
 function Home(props) {
   const [loading, setLoading] = useState(false)
   
-  const buttonStyles = {
-    backgroundColor: '#ff0000',
-    color: 'white',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    margin: '5px',
-  };
 
 
   return (
@@ -27,23 +18,19 @@ function Home(props) {
         <Image src="/images/WhiteLogo.png" alt="logo" width={300} height={80} />
 
         <Link href={`/recipes/1`} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '30px' }}>
-          <button style={buttonStyles} onClick={() => setLoading(true)}>All Recipe</button>
+          <button className={styles.buttonStyles} onClick={() => setLoading(true)}>All Recipe</button>
         </Link>
      
         {/* Login and Sign Up buttons with links */}
         <Link href="/login" style={{ textDecoration: 'none' }}>
-          <button style={buttonStyles}>Login</button>
+          <button className={styles.buttonStyles}>Login</button>
         </Link>
         <Link href="/signup" style={{ textDecoration: 'none' }}>
-          <button style={buttonStyles}>Sign Up</button>
+          <button className={styles.buttonStyles}>Sign Up</button>
         </Link>
        
       </div>
-      <div className={styles.background}
-        style={{
-          backgroundImage: 'url("/images/homepage-pic.jpg")',
-        }}
-      >
+      <div className={styles.background}>
 
       </div>
     </div>
